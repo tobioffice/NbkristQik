2dde2d16e6f0737be9cd71b66dbb9065385f0c1e
tobioffice <muraliaggipothu@gmail.com>
2025-12-31 17:37:40 +0530

    Format code for consistency and improve readability in db.ts and temp.ts

--- src/db/temp.ts
+++ src/db/temp.ts
@@ -1,14 +1,14 @@
 import { turso } from "./db.js";
 
 turso
-  .execute(
-    `
+   .execute(
+      `
     CREATE TABLE IF NOT EXISTS fallbackResponses (
         id TEXT PRIMARY KEY CHECK(LENGTH(id) <= 50),
         content TEXT
     );
-`,
-  )
-  .then((res) => {
-    console.log("Fallback Responses Table ensured", res);
-  });
+`
+   )
+   .then((res) => {
+      console.log("Fallback Responses Table ensured", res);
+   });
